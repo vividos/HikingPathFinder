ceaf7a1198a343e06dbf4c4be1b91f9df2834e63
Michael Fink <michael.fink@asamnet.de>
2017-04-07 18:06:44 +0200

    updated explore map js code for Windows Phone

--- src/Frontend/App/WindowsPhone/Assets/map/map.js
+++ src/Frontend/App/WindowsPhone/Assets/map/map.js
@@ -6,17 +6,18 @@ function updateMyPosition(options) {
 
     console.log("updating my position: lat=" + options.latitude + ", long=" + options.longitude);
 
-    myPositionMarker.setLatLng(options.latitude, options.longitude);
+    myPositionMarker.setLatLng([options.latitude, options.longitude]);
     myPositionMarker.addTo(map);
+    myPositionMarker.update();
 
     if (options.zoomTo) {
         console.log("also zooming to my position");
-        myPositionMarker.zoomTo();
+        map.panTo([options.latitude, options.longitude]);
     }
 }
 
 function zoomToLocation(options) {
 
     console.log("zooming to: lat=" + options.latitude + ", long=" + options.longitude);
-    map.zoomTo(options.latitude, options.longitude);
+    map.panTo([options.latitude, options.longitude]);
 }

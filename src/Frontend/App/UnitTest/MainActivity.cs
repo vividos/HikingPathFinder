b21ddaec7ea2be7accd4520feb9541d9d48f2663
Michael Fink <michael.fink@asamnet.de>
2017-04-23 09:48:05 +0200

    added Android unit test app

---
+++ src/Frontend/App/UnitTest/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.OS;
+using GalaSoft.MvvmLight.Ioc;
+using HikingPathFinder.App;
+using HikingPathFinder.App.Android;
+using HikingPathFinder.App.Database;
+using Microsoft.Practices.ServiceLocation;
+using System.Reflection;
+using Xamarin.Android.NUnitLite;
+
+namespace UnitTest
+{
+    /// <summary>
+    /// Unit test main activity for Android
+    /// </summary>
+    [Activity(Label = "HikingPathFinder UnitTests for Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : TestSuiteActivity
+    {
+        /// <summary>
+        /// Called when activity is about to be created
+        /// </summary>
+        /// <param name="bundle">bundle parameter; unused</param>
+        protected override void OnCreate(Bundle bundle)
+        {
+            this.InitServiceLocator();
+
+            // tests can be inside the main assembly
+            this.AddTest(Assembly.GetExecutingAssembly());
+            //// or in any reference assemblies
+            //// AddTest (typeof(Your.Library.TestClass).Assembly);
+
+            // Once you called base.OnCreate(), you cannot add more assemblies.
+            base.OnCreate(bundle);
+        }
+
+        /// <summary>
+        /// Initializes service locator used throughout the unit tests; uses MvvmLight's
+        /// SimpleIoc.
+        /// </summary>
+        private void InitServiceLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<ISQLiteDatabaseProvider, AndroidSQLiteDatabaseProvider>();
+            SimpleIoc.Default.Register<IPlatform, AndroidPlatform>();
+        }
+    }
+}
---
+++ src/Frontend/App/UnitTest/TestDatabase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace HikingPathFinder.App.UnitTest
+{
+    /// <summary>
+    /// Tests for Database classes
+    /// </summary>
+    [TestFixture]
+    public class TestDatabase
+    {
+        /// <summary>
+        /// Tests default ctor of Database class
+        /// </summary>
+        [Test]
+        public void TestDefaultCtor()
+        {
+            var platform = ServiceLocator.Current.GetInstance<IPlatform>();
+
+            string databaseFilename = platform.PathCombine(platform.AppDataFolder, "newdatabase.db");
+
+            using (var database = new HikingPathFinder.App.Database.Database(databaseFilename))
+            {
+                var connection = database.GetConnection();
+
+                int libVersionNumber = connection.Platform.SQLiteApi.LibVersionNumber();
+
+                Debug.WriteLine("LibVersionNumber = {0}", libVersionNumber);
+            }
+        }
+    }
+}

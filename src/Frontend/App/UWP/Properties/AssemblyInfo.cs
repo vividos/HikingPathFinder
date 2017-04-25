677f7e5e5cb11ebf433983f9caf4fe753422d6a1
Michael Fink <michael.fink@asamnet.de>
2017-04-25 21:34:46 +0200

    added imagesa and assets, using shared version assembly info, and changed some build settings

--- src/Frontend/App/UWP/Properties/AssemblyInfo.cs
+++ src/Frontend/App/UWP/Properties/AssemblyInfo.cs
@@ -1,29 +1,8 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HikingPathFinder.App.UWP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("HikingPathFinder Windows 10 app")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HikingPathFinder.App.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: ComVisible(false)]
